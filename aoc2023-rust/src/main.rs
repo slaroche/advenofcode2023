ca4cd87a98e79a2aec0636bd8a93dfeea6d2a6a8
Samuel Laroche <slaroche.me@gmail.com>
2023-12-12 08:16:49 -0500

    rust arg parser

--- aoc2023-rust/src/main.rs
+++ aoc2023-rust/src/main.rs
@@ -1,22 +1,55 @@
-mod runner;
 mod day_10;
-
-
 use std::env;
 
 
-fn main() {
-    runner::hello_world();
-    day_10::solver::solve();
-    let args: Vec<String> = env::args().collect();
+#[derive(Debug)]
+struct Command {
+    example: bool,
+    day: i32,
+    input_file: String,
+    part: String,
+}
 
-    for arg in &args {
+impl Default for Command {
+    fn default() -> Self {
+        Command {
+            example: false,
+            day:1,
+            input_file:String::from(""),
+            part: String::from("all"),
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // let _ = day_10::solver::solve();
+    let mut skip = Vec::new();
+    let mut command = Command::default();
+    for (i, arg) in args.iter().enumerate() {
+        if skip.contains(&i) || i == 0 {
+            continue;
+        }
         match arg.as_str() {
-            "-p" => println!("allo"),
+            "-p" | "--part" => {
+                command.part = args[i + 1].to_string();
+                skip.push(i + 1);
+            },
+            "-e" | "--example" => {
+                command.example = true;
+            },
+            "-i" | "--input-file" => {
+                command.input_file = args[i + 1].to_string();
+                skip.push(i + 1);
+            },
+            "-d" | "--day" => {
+                command.day = args[i + 1].parse::<i32>().unwrap();
+                skip.push(i + 1);
+            },
             _ => println!("chose"),
         }
     }
 
-    println!("args: {:?}",  &args[0..]);
+    println!("args: {:?}, {:?}",  &args[0..], command);
 
 }
